b585b766e7c4aafe5fd4a509c35530857c8c6470
AyushiK16 <ayushikaushal16@gmail.com>
2021-01-10 11:24:03 +0530

    HI

--- components/Barter.js
+++ components/Barter.js
@@ -4,6 +4,8 @@ import LottieView from 'lottie-react-native';
 export default class Barter extends React.Component{
     render(){
         return(
+            //this is a comment!
+            
             <LottieView
             source = {require('../assets/barterLottie.json')}
             style = {{width : '60%'}}
